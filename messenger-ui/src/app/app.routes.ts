38701a6fd426b94fabc252db7ea476f88fb3d8a0
Abraham Guzman <abraham.guz95@gmail.com>
2026-07-09 12:13:32 -0600

    Agregar messenger-ui como carpeta normal

---
+++ messenger-ui/src/app/app.routes.ts
@@ -0,0 +1,28 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./features/auth/login/login.component').then(m => m.LoginComponent)
+  },
+  {
+    path: 'register',
+    loadComponent: () =>
+      import('./features/auth/register/register.component').then(m => m.RegisterComponent)
+  },
+  {
+    path: 'home',
+    loadComponent: () =>
+      import('./features/dashboard/home/home.component').then(m => m.HomeComponent)
+  },
+  {
+    path: '**',
+    redirectTo: 'login'
+  }
+];
